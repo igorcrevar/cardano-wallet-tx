d0cc74dcdc326587bb3aa738addd86391bc12fda
Igor Crevar <crewce@gmail.com>
2024-05-17 14:56:02 +0200

    check outputs for min amount

--- core/transaction_test.go
+++ core/transaction_test.go
@@ -3,6 +3,8 @@ package core
 import (
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,7 +18,7 @@ var (
 func TestTransactionBuilder(t *testing.T) {
 	const (
 		testNetMagic = 203
-		ttl          = 28096
+		ttl          = uint64(28096)
 	)
 
 	walletsKeyHashes := []string{
@@ -46,11 +48,26 @@ func TestTransactionBuilder(t *testing.T) {
 	multiSigFeeAddr, err := policyScriptFeeMultiSig.CreateMultiSigAddress(testNetMagic)
 	require.NoError(t, err)
 
-	metadata := map[string]interface{}{
-		"0": map[string]interface{}{
-			"type":       "multi",
-			"signers":    len(walletsKeyHashes),
-			"feeSigners": len(walletsFeeKeyHashes),
+	type metaDataKey0 struct {
+		Type       string `json:"type" cbor:"type"`
+		Signers    int    `json:"signers" cbor:"signers"`
+		FeeSigners int    `json:"feeSigners" cbor:"feeSigners"`
+	}
+
+	type metaDataKey1 struct {
+		Company string `json:"comp" cbor:"comp"`
+		City    string `json:"city" cbor:"city"`
+	}
+
+	metadata := map[uint64]interface{}{
+		0: metaDataKey0{
+			Type:       "multi",
+			Signers:    len(walletsKeyHashes),
+			FeeSigners: len(walletsFeeKeyHashes),
+		},
+		4: metaDataKey1{
+			Company: "Ethernal",
+			City:    "Novi Sad",
 		},
 	}
 	outputs := []TxOutput{
@@ -80,7 +97,7 @@ func TestTransactionBuilder(t *testing.T) {
 				Index: 2,
 			},
 		},
-		Sum: MinUTxODefaultValue + 20,
+		Sum: MinUTxODefaultValue*3 - 10,
 	}
 
 	multiSigFeeInputs := TxInputs{
@@ -90,7 +107,7 @@ func TestTransactionBuilder(t *testing.T) {
 				Index: 0,
 			},
 		},
-		Sum: MinUTxODefaultValue,
+		Sum: MinUTxODefaultValue * 2,
 	}
 
 	builder.SetTimeToLive(ttl).SetProtocolParameters(protocolParameters)
@@ -114,8 +131,11 @@ func TestTransactionBuilder(t *testing.T) {
 	txRaw, txHash, err := builder.Build()
 	require.NoError(t, err)
 
-	assert.Equal(t, "84a50083825820098236134e0f2077a6434dd9d7727126fa8b3627bcab3ae030a194d46eded73e00825820d1fd0d772be7741d9bfaf0b037d02d2867a987ccba3e6ba2ee9aa2a861b7314502825820e99a5bde15aa05f24fcc04b7eabc1520d3397283b1ee720de9fe2653abbb0c9f00018382581d60244877c1aeefc7fd5405a6e14d927d91758d45e37c20fa2ac89cb1671a000f424082581d700c25e4ff24cfa0dfebcec382095161271dc9bb744ca4149ec604dc991482581d70a5caf9ce4bed09c794ee87bddb6505822db5bd476a4f61e0cd4074a21a000b3ca7021a0004059903196dc0075820981cfc0e6c2095e5e630840d9e8c078a2d13677ac5f97d8945d8d0c46a53047ca10182830304858200581cd6b67f93ffa4e2651271cc9bcdbdedb2539911266b534d9c163cba218200581ccba89c7084bf0ce4bf404346b668a7e83c8c9c250d1cafd8d8996e418200581c79df3577e4c7d7da04872c2182b8d8829d7b477912dbf35d89287c398200581c2368e8113bd5f32d713751791d29acee9e1b5a425b0454b963b2558b8200581c06b4c7f5254d6395b527ac3de60c1d77194df7431d85fe55ca8f107d830304858200581cf0f4837b3a306752a2b3e52394168bc7391de3dce11364b723cc55cf8200581c47344d5bd7b2fea56336ba789579705a944760032585ef64084c92db8200581cf01018c1d8da54c2f557679243b09af1c4dd4d9c671512b01fa5f92b8200581c6837232854849427dae7c45892032d7ded136c5beb13c68fda635d878200581cd215701e2eb17c741b9d306cba553f9fbaaca1e12a5925a065b90fa8f5d90103a100a100a36a6665655369676e65727305677369676e657273056474797065656d756c7469", hex.EncodeToString(txRaw))
-	assert.Equal(t, "371837ccfdfbf3ecdcc0fdee26c2e349aa988402bfdecd130a852d799d07bb04", txHash)
+	fmt.Println(txHash)
+	fmt.Println(hex.EncodeToString(txRaw))
+
+	assert.Equal(t, "84a50083825820098236134e0f2077a6434dd9d7727126fa8b3627bcab3ae030a194d46eded73e00825820d1fd0d772be7741d9bfaf0b037d02d2867a987ccba3e6ba2ee9aa2a861b7314502825820e99a5bde15aa05f24fcc04b7eabc1520d3397283b1ee720de9fe2653abbb0c9f00018382581d60244877c1aeefc7fd5405a6e14d927d91758d45e37c20fa2ac89cb1671a000f424082581d700c25e4ff24cfa0dfebcec382095161271dc9bb744ca4149ec604dc991a001e847682581d70a5caf9ce4bed09c794ee87bddb6505822db5bd476a4f61e0cd4074a21a001a79bf021a00040ac103196dc0075820802e4d6f15ce98826886a5451e94855e77aae779cb341d3aab1e3bae4fb2f78da10182830304858200581cd6b67f93ffa4e2651271cc9bcdbdedb2539911266b534d9c163cba218200581ccba89c7084bf0ce4bf404346b668a7e83c8c9c250d1cafd8d8996e418200581c79df3577e4c7d7da04872c2182b8d8829d7b477912dbf35d89287c398200581c2368e8113bd5f32d713751791d29acee9e1b5a425b0454b963b2558b8200581c06b4c7f5254d6395b527ac3de60c1d77194df7431d85fe55ca8f107d830304858200581cf0f4837b3a306752a2b3e52394168bc7391de3dce11364b723cc55cf8200581c47344d5bd7b2fea56336ba789579705a944760032585ef64084c92db8200581cf01018c1d8da54c2f557679243b09af1c4dd4d9c671512b01fa5f92b8200581c6837232854849427dae7c45892032d7ded136c5beb13c68fda635d878200581cd215701e2eb17c741b9d306cba553f9fbaaca1e12a5925a065b90fa8f5d90103a100a200a36a6665655369676e65727305677369676e657273056474797065656d756c746904a26463697479684e6f76692053616464636f6d706845746865726e616c", hex.EncodeToString(txRaw))
+	assert.Equal(t, "57e7d44e269186ede48e1d8892ae290c857d9eaad6c7eadecc3f90205a486d17", txHash)
 }
 
 func Test_TxBuilder_UpdateOutputAmountAndRemoveOutput(t *testing.T) {
@@ -170,4 +190,47 @@ func Test_TxBuilder_UpdateOutputAmountAndRemoveOutput(t *testing.T) {
 	builder.RemoveOutput(0)
 
 	require.Len(t, builder.outputs, 0)
+}
+
+func Test_TxBuilder_CheckOutputs(t *testing.T) {
+	b, err := NewTxBuilder()
+	require.NoError(t, err)
+
+	defer b.Dispose()
+
+	b.AddOutputs(TxOutput{
+		Addr:   "x1",
+		Amount: MinUTxODefaultValue,
+	}, TxOutput{
+		Addr:   "x2",
+		Amount: MinUTxODefaultValue + 1,
+	})
+
+	require.NoError(t, b.CheckOutputs())
+
+	b.AddOutputs(TxOutput{
+		Addr:   "x3",
+		Amount: MinUTxODefaultValue - 1,
+	}, TxOutput{
+		Addr:   "x4",
+		Amount: MinUTxODefaultValue - 2,
+	})
+
+	err = b.CheckOutputs()
+	require.Error(t, err)
+
+	require.True(t, strings.Contains(err.Error(), "output (2, x3) has insufficient amount 999999"))
+	require.True(t, strings.Contains(err.Error(), "output (3, x4) has insufficient amount 999998"))
+
+	b.SetMinOutputAmount(MinUTxODefaultValue - 1)
+
+	err = b.CheckOutputs()
+	require.Error(t, err)
+
+	require.False(t, strings.Contains(err.Error(), "output (2, x3) has insufficient amount 999999"))
+	require.True(t, strings.Contains(err.Error(), "output (3, x4) has insufficient amount 999998"))
+
+	b.SetMinOutputAmount(MinUTxODefaultValue - 2)
+
+	require.NoError(t, b.CheckOutputs())
 }
