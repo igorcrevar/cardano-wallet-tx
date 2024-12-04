a6def7b98f1b3d152176b97ad578b8cd8ee4c9b7
Igor Crevar <crewce@gmail.com>
2024-12-04 12:33:20 +0100

    cardano address refactor + protocol parameters changes

--- core/network_type.go
+++ core/network_type.go
@@ -8,23 +8,12 @@ const (
 	TestNetProtocolMagic = uint(1097911063)
 	MainNetProtocolMagic = uint(764824073)
 
-	PrimeTestNetProtocolMagic  = uint(3311)
-	PrimeMainNetProtocolMagic  = uint(764824073)
-	VectorTestNetProtocolMagic = uint(1127)
-	VectorMainNetProtocolMagic = uint(3327)
-
-	VectorMainNetNetwork CardanoNetworkType = 3
-	VectorTestNetNetwork CardanoNetworkType = 2
-	MainNetNetwork       CardanoNetworkType = 1
-	TestNetNetwork       CardanoNetworkType = 0
+	MainNetNetwork CardanoNetworkType = 1
+	TestNetNetwork CardanoNetworkType = 0
 )
 
 func (n CardanoNetworkType) GetPrefix() string {
 	switch n {
-	case VectorTestNetNetwork:
-		return "vector_test"
-	case VectorMainNetNetwork:
-		return "vector"
 	case MainNetNetwork:
 		return "addr"
 	case TestNetNetwork:
@@ -36,9 +25,9 @@ func (n CardanoNetworkType) GetPrefix() string {
 
 func (n CardanoNetworkType) GetStakePrefix() string {
 	switch n {
-	case MainNetNetwork, VectorMainNetNetwork:
+	case MainNetNetwork:
 		return "stake"
-	case TestNetNetwork, VectorTestNetNetwork:
+	case TestNetNetwork:
 		return "stake_test"
 	default:
 		return "" // not handled but dont raise an error
@@ -50,7 +39,5 @@ func (n CardanoNetworkType) IsMainNet() bool {
 }
 
 func IsAddressWithValidPrefix(addr string) bool {
-	return strings.HasPrefix(addr, "addr") ||
-		strings.HasPrefix(addr, "vector") ||
-		strings.HasPrefix(addr, "stake")
+	return strings.HasPrefix(addr, "addr") || strings.HasPrefix(addr, "stake")
 }
